e55c510989a2b5f851065bdd71894ad6cb4aa699
Orlando Villaseñor <orlandovillasenor@me.com>
2014-04-26 16:13:58 -0500

    updated users table and model for remember_token

--- app/database/migrations/2014_03_28_190155_create_users_table.php
+++ app/database/migrations/2014_03_28_190155_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration {
             $table->increments('id');
             $table->string('email', 200)->unique();
             $table->string('password', 100);
+            $table->string('remember_token', 100)->nullable();;
             $table->timestamps();
         });
 	}
